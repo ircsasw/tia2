071d0f15daa16c7bdbf2484ffe646c617a046d8c
Hillary98 <hillary_princesa@hotmail.com>
2015-03-11 07:48:20 -0600

    INDEX

--- index.php
+++ index.php
@@ -61,10 +61,10 @@ $Link) or die(mysql_error());
 			<td colspan="4"> <input type="submit" name="Me gusta" value="Me gusta"> </td>
 		</tr>
 		<tr>
-			<td colspan="3"> Usuario y fecha </td> <td> <input type="submit" name="editar" value="editar"> <input type="submit" name="borrar" value="borrar"> </td>
+			<td colspan="3"> <?php echo $row["id_usuario"]; ?> </td> <td> <input type="submit" name="editar" value="editar"> <input type="submit" name="borrar" value="borrar"> </td>
 		</tr>
 		<tr>
-			<td colspan="4"> comentario </td>
+			<td colspan="4"> <?php echo $row["contenido"]; ?>  </td>
 		</tr>
 	</table>
 
