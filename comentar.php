64bb481e56292c83df9340d7e4aa15791b6ca273
Arturo <ircsasw@gmail.com>
2015-03-13 08:37:23 -0500

    comentario

---
+++ comentar.php
@@ -0,0 +1,14 @@
+<?php
+ echo "esto";
+ ?>
+ <html>
+ 	<head>
+ 		<title>comentario</title>
+ 	</head>
+ 	<body>
+ 	<form action="index.php" method="post">
+ 		<textarea name="comentario" > </textarea>
+ 		<input type="submit" name="comentar" value="comentar">
+ 		</form>
+ 	</body>
+ </html>
